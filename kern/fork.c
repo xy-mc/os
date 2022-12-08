f25a55a1a2948f00722323166782d2b5009be08a
xymc <2083054765@qq.com>
2022-12-08 16:32:41 +0800

    2

--- kern/fork.c
+++ kern/fork.c
@@ -1,13 +1,68 @@
 #include <assert.h>
 #include <string.h>
-
+#include <x86.h>
 #include <kern/fork.h>
 #include <kern/syscall.h>
 #include <kern/trap.h>
 #include <kern/kmalloc.h>
 #include <kern/process.h>
 #include <kern/pmap.h>
+#include <kern/sche.h>
+#include <kern/stdio.h>
 u32 pid=1;
+phyaddr_t
+alloc_phy_page(struct page_node **page_list)
+{
+	phyaddr_t paddr = phy_malloc_4k();
+	
+	struct page_node *new_node = kmalloc(sizeof(struct page_node));
+	new_node->nxt = *page_list;
+	new_node->paddr = paddr;
+	new_node->laddr = -1;
+	*page_list = new_node;
+
+	return paddr;
+}
+void
+lin_mapping_phy1(u32			cr3,
+		struct page_node	**page_list,
+		uintptr_t		laddr,
+		phyaddr_t		paddr,
+		u32			pte_flag)
+{
+	assert(PGOFF(laddr) == 0);
+
+	uintptr_t *pde_ptr = (uintptr_t *)K_PHY2LIN(cr3);
+
+	if ((pde_ptr[PDX(laddr)] & PTE_P) == 0) {
+		phyaddr_t pte_phy = alloc_phy_page(page_list);
+		memset((void *)K_PHY2LIN(pte_phy), 0, PGSIZE);
+		pde_ptr[PDX(laddr)] = pte_phy | PTE_P | PTE_W | PTE_U;
+	}
+
+	phyaddr_t pte_phy = PTE_ADDR(pde_ptr[PDX(laddr)]);
+	uintptr_t *pte_ptr = (uintptr_t *)K_PHY2LIN(pte_phy);
+
+	phyaddr_t page_phy;
+	if (paddr == (phyaddr_t)-1) {
+		if ((pte_ptr[PTX(laddr)] & PTE_P) != 0)
+			return;
+		page_phy = alloc_phy_page(page_list);
+		(*page_list)->laddr = laddr;
+		
+	} else {
+		if ((pte_ptr[PTX(laddr)] & PTE_P) != 0)
+			warn("this page was mapped before, laddr: %x", laddr);
+		assert(PGOFF(paddr) == 0);
+		page_phy = paddr;
+		// struct page_node *new_node = kmalloc(sizeof(struct page_node));
+		// new_node->nxt = *page_list;
+		// new_node->paddr = paddr;
+		// new_node->laddr = -1;
+		// *page_list = new_node;
+	}
+	pte_ptr[PTX(laddr)] = page_phy | pte_flag;
+}
 ssize_t
 kern_fork(PROCESS_0 *p_fa)
 {
@@ -22,21 +77,31 @@ kern_fork(PROCESS_0 *p_fa)
 
 	// fork的第一步你需要找一个空闲（IDLE）的进程作为你要fork的子进程
 	//panic("Unimplement! find a idle process");
-	for(int i=0;i<PCB_SIZE;i++)
+	PROCESS *p_proc_idle;
+	int i;
+	for(i=0;i<PCB_SIZE;i++)
 		if(proc_table[i].pcb.statu==IDLE)
-			p_proc_ready=proc_table+i;
+			p_proc_idle=proc_table+i;
+	if(i==PCB_SIZE)
+		return -1;
 	// 再之后你需要做的是好好阅读一下pcb的数据结构，搞明白结构体中每个成员的语义
 	// 别光扫一遍，要搞明白这个成员到底在哪里被用到了，具体是怎么用的
 	// 可能exec和exit系统调用的代码能够帮助你对pcb的理解，不先理解好pcb你fork是无从下手的
 	//panic("Unimplement! read pcb");
-	PROCESS_0 *p_proc = &p_proc_ready->pcb;
+	PROCESS_0 *p_proc = &p_proc_idle->pcb;
+	while (xchg(&p_fa->lock, 1) == 1)
+		schedule();
+	while (xchg(&p_proc->lock, 1) == 1)
+		schedule();
 	p_proc->kern_regs.esp = (u32)(p_proc_ready + 1) - 8;
 	// 保证切换内核栈后执行流进入的是restart函数。
 	*(u32 *)(p_proc->kern_regs.esp + 0) = (u32)restart;
 	// 这里是因为restart要用`pop esp`确认esp该往哪里跳。
 	*(u32 *)(p_proc->kern_regs.esp + 4) = (u32)p_proc;
-	p_fa->pid=0;
+	//p_fa->pid=0;
 	p_proc->pid=pid++;
+	kprintf("%d\n",p_proc->pid);
+	//p_fa->pid=p_proc->pid;
 	p_proc->priority=p_fa->priority;
 	p_proc->ticks=p_fa->ticks;
 	memcpy(&p_proc->user_regs, &p_fa->user_regs, sizeof(p_proc->user_regs));
@@ -46,6 +111,8 @@ kern_fork(PROCESS_0 *p_fa)
 	new_page_list->nxt = NULL;
 	new_page_list->paddr = new_cr3;
 	new_page_list->laddr = -1;
+	map_kern(new_cr3, &new_page_list);
+	DISABLE_INT();
 	p_proc->cr3=new_cr3;
 	p_proc->page_list=new_page_list;
 	//panic("Unimplement! copy pcb?");
@@ -54,11 +121,12 @@ kern_fork(PROCESS_0 *p_fa)
 		if(p->laddr==-1)
 			continue;
 		struct page_node *new_list = kmalloc(sizeof(struct page_node));
-		lin_mapping_phy(new_cr3,&new_list,p->laddr,(phyaddr_t)-1,PTE_P | PTE_W | PTE_U);
 		new_list->nxt=NULL;
-		new_list->laddr=p->laddr;
+		lin_mapping_phy1(new_cr3,&new_list,p->laddr,(phyaddr_t)-1,PTE_P | PTE_W | PTE_U);
 		p_proc->page_list->nxt=new_list;
 	}
+	lcr3(p_proc->cr3);
+	ENABLE_INT();
 	//panic("Unimplement! copy pcb?");
 	
 	// 在阅读完pcb之后终于可以开始fork工作了
@@ -90,8 +158,15 @@ kern_fork(PROCESS_0 *p_fa)
 	// 2. 所有错误情况都判断到了吗？错误情况怎么处理？（RTFM->`man 2 fork`）
 	// 3. 你写的代码真的符合fork语义吗？
 	panic("Unimplement! soul torture");
-
-	return 0;
+free:
+	xchg(&p_proc->lock, 0);
+	xchg(&p_fa->lock, 0);
+	//return kern_get_pid(p_proc);
+	// if(p_proc_ready->pcb.pid==p_fa->pid)
+	// 	return p_proc->pid;
+	// else
+	// 	return 0;
+	return p_proc->pid;
 }
 
 ssize_t
